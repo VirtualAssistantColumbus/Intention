04f545c38504df4fe3ea67d69fd47093be1d8fae
Ray Li <rl.invent@gmail.com>
2025-07-09 23:00:11 -0400

    updated post-sign-up question to be more succinct and forward.

--- pages/boredom_letter_2.py
+++ pages/boredom_letter_2.py
@@ -165,27 +165,28 @@ def render_boredom_letter_2_post_sign_up() -> str:
         <!-- Question Header -->
         <section class="px-8 pt-16 pb-8 md:px-16 lg:px-24">
             <h1 class="text-massive-mobile md:text-massive font-bold mb-10 leading-none tracking-tight">
-                Share your thoughts.
+                Thank you and quick question
             </h1>
 
             <div class="max-w-4xl space-y-8">
-                <p class="text-xl md:text-2xl leading-relaxed">
-                    Thank you for joining our mission to make technology serve humanity again.
-                    </p>
-                    <p class="text-xl md:text-2xl leading-relaxed font-bold">
-                        What was it about our mission that resonated with you?
-                    </p>
+                <p class="text-xl md:text-2xl leading-relaxed font-bold">
+                    What was it about our mission that resonated with you?
+                </p>
                 
                 <p class="text-xl md:text-2xl leading-relaxed">
-                    We just <strong>sent you an email</strong> with this question in it, and we'd love to hear your thoughts.
-                </p>
-                <p class="text-xl md:text-2xl leading-relaxed">
+                    We just <strong>sent you an email</strong> with this question in it.
                     If you have a minute, could you <strong>write us a response</strong> while this is top of mind?
                 </p>
                 <p class="text-xl md:text-2xl leading-relaxed">
-                    We personally read every response, and your thoughts will help inform our product roadmap.<br><br>
-                    Thank you,<br>
+                    We personally read every message, and your thoughts will help inspire our design ethos.
+                </p>
+                <p class="text-xl md:text-2xl leading-relaxed">
+                    Thank you for joining our mission to make technology serve humanity again.
+                </p>
+                <p class="text-xl md:text-2xl leading-relaxed">
+                    Sincerely,<br>
                     The Intention Team
+                </p>
                    
         { nav() }
     """)
